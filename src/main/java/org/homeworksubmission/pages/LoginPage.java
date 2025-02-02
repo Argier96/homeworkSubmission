5079024b4e6d0b6fa05cb97fa68b9ad196beedbd
argier96 <argier96@gmail.com>
2025-02-02 07:02:38 +0200

    login page created

--- src/main/java/org/homeworksubmission/pages/LoginPage.java
+++ src/main/java/org/homeworksubmission/pages/LoginPage.java
@@ -1,4 +1,4 @@
-package org.homeworksubmission;
+package org.homeworksubmission.pages;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -7,11 +7,11 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class LoginPage extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("loginScreenView.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        FXMLLoader fxmlLoader = new FXMLLoader(org.homeworksubmission.HelloApplication.class.getResource("loginScreenView.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
         stage.setTitle("Homework Submission");
         stage.setScene(scene);
         stage.show();
