7ec8c59d002358d67b2f4909c86291ffb5dc94c3
argier96 <argier96@gmail.com>
2025-02-02 14:05:02 +0200

    Forgot password page created

--- src/main/java/org/homeworksubmission/pages/LoginPage.java
+++ src/main/java/org/homeworksubmission/pages/LoginPage.java
@@ -15,6 +15,8 @@ public class LoginPage extends Application {
         stage.setTitle("Homework Submission");
         stage.setScene(scene);
         stage.show();
+
+
     }
 
     public static void main(String[] args) {
